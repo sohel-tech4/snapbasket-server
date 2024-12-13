e6daf46f21041b3f68c8f0b9262ca85b0d02025d
sohel-tech4 <soheltech4@gmail.com>
2024-12-14 01:18:13 +0600

    edit

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 const app = express();
-const port = 3000;
+// const port = 3000;
 
 //parsers
 app.use(express.json());
