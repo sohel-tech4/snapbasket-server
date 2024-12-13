fb8dd4f9fd5e57b1142d7b5854bc0980a5016f3f
sohel-tech4 <soheltech4@gmail.com>
2024-12-14 02:14:40 +0600

    add model

---
+++ src/app/modules/student/student.schema.ts
@@ -0,0 +1,52 @@
+import { Schema, model } from "mongoose";
+import {
+  Guardian,
+  LocalGuardian,
+  Student,
+  UserName,
+} from "./student.interface";
+
+const UserNameSchema = new Schema<UserName>({
+  firstName: { type: String, required: true },
+  middleName: { type: String, required: false }, // Optional
+  lastName: { type: String, required: true },
+});
+
+const GuardianSchema = new Schema<Guardian>({
+  fatherName: { type: String, required: true },
+  fatherOccupation: { type: String, required: true },
+  fatherContact: { type: String, required: true },
+  motherName: { type: String, required: true },
+  motherOccupation: { type: String, required: true },
+  motherContact: { type: String, required: true },
+});
+
+const LocalGuardianSchema = new Schema<LocalGuardian>({
+  name: { type: String, required: true },
+  occupation: { type: String, required: true },
+  contactNo: { type: String, required: true },
+  address: { type: String, required: true },
+});
+
+const StudentSchema = new Schema<Student>({
+  id: { type: String },
+  name: { type: UserNameSchema, required: true },
+  gender: { type: String, enum: ["male", "female"], required: true },
+  dateOfBirth: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  contactNo: { type: String, required: true },
+  emergencyContactNo: { type: String, required: true },
+  bloodGroup: {
+    type: String,
+    enum: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+    required: false,
+  },
+  presentAddress: { type: String, required: true },
+  permanentAddress: { type: String, required: true },
+  guardian: { type: GuardianSchema, required: true },
+  localGuardian: { type: LocalGuardianSchema, required: true },
+  profileImg: { type: String, required: false }, // Optional
+  isActive: { type: String, enum: ["active", "inActive"], required: true },
+});
+
+const Student = model<Student>("Student", StudentSchema);
