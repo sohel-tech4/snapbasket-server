cf25f2ef6442b0719a73bf9e624151cbe05f2abf
sohel-tech4 <soheltech4@gmail.com>
2024-12-24 08:37:13 +0600

    add

--- src/app/modules/user/user.sevice.ts
+++ src/app/modules/user/user.sevice.ts
@@ -2,9 +2,9 @@ import { TStudent } from "../student/student.interface";
 import { Student } from "../student/student.model";
 
 const createStudentIntoDB = async (studentData: TStudent) => {
-  if (await Student.isUserExists(studentData.id)) {
-    throw new Error("User Already Exists");
-  }
+  //   if (await Student.isUserExists(studentData.id)) {
+  //     throw new Error("User Already Exists");
+  //   }
   const result = await Student.create(studentData);
   return result;
 };
