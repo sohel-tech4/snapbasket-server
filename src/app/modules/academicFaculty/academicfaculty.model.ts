3c2122fd86055326ba4f67c8bad24e662ef10f74
Sohel Rana <soheltech4@gmail.com>
2025-01-04 12:59:08 +0600

    add pre hook for check same faculty and department

--- src/app/modules/academicFaculty/academicfaculty.model.ts
+++ src/app/modules/academicFaculty/academicfaculty.model.ts
@@ -8,6 +8,28 @@ const academicFacultySchema = new Schema<TAcademicFaculty>({
   },
 });
 
+academicFacultySchema.pre("save", async function(next){
+ const isFacultyExist = await AcademicFacultyModel.findOne({
+  name : this.name
+ }) 
+ if(isFacultyExist){
+  throw new Error("Faculty already exist")
+ }
+ next()
+})
+
+academicFacultySchema.pre("findOneAndUpdate", async function(next){
+  const query = this.getQuery()
+  const isFacultyExist = await AcademicFacultyModel.findOne({
+    query
+   }) 
+   if(!isFacultyExist){
+    throw new Error("This Faculty Does Not exist")
+   }
+   next()
+})
+
+
 export const AcademicFacultyModel = model<TAcademicFaculty>(
   "Academicfaculty",
   academicFacultySchema
--- src/app/modules/academicdDepartment/academicdepartment.model.ts
+++ src/app/modules/academicdDepartment/academicdepartment.model.ts
@@ -3,7 +3,11 @@ import { TacademicDepartment } from "./academicdepartment.interface";
 
 const academicDepartmentSchema = new Schema<TacademicDepartment>(
   {
-    name: { type: String, required: true, trim: true },
+    name: { 
+      type: String, 
+      required: true, 
+      unique: true 
+    },
     academicfaculty: {
       type: Schema.Types.ObjectId,
       ref: "AcademicFaculty",
@@ -14,6 +18,29 @@ const academicDepartmentSchema = new Schema<TacademicDepartment>(
     timestamps: true,
   }
 );
+
+// for create
+academicDepartmentSchema.pre("save", async function(next){
+const isDepartmentExist = await AcademicDepartmentModel.findOne({
+  name: this.name
+})
+if(isDepartmentExist){
+  throw new Error("Department already exist")
+}
+next()
+})
+
+// for update
+academicDepartmentSchema.pre("findOneAndUpdate", async function(next){
+const query = this.getQuery()
+const isDepartmentExist = await AcademicDepartmentModel.findOne({query
+})
+if(!isDepartmentExist){
+  throw new Error("This Department Does not exist")
+}
+next()
+})
+
 
 export const AcademicDepartmentModel = model<TacademicDepartment>(
   "Academicdepartmetn",
